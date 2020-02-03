8a5de0ac197c3f0af3ce9bfe9739b4df69d3f5fb
EchoCow <lizhongyue248@163.com>
2020-02-03 19:08:50 +0800

    :ok_hand: Updating code due to code review changes.

---
+++ src/main/kotlin/cn/edu/gzmu/center/model/Address.kt
@@ -0,0 +1,9 @@
+package cn.edu.gzmu.center.model
+
+/**
+ * .
+ *
+ * @author <a href="https://echocow.cn">EchoCow</a>
+ * @date 2020/2/2 下午11:35
+ */
+const val CONFIG_ADDRESS = "authorization-center-config"
--- src/main/kotlin/cn/edu/gzmu/center/verticle/DatabaseVerticle.kt
+++ src/main/kotlin/cn/edu/gzmu/center/verticle/DatabaseVerticle.kt
@@ -1,11 +1,20 @@
 package cn.edu.gzmu.center.verticle
 
 import cn.edu.gzmu.center.config.ApplicationConfig
+import cn.edu.gzmu.center.model.CONFIG_ADDRESS
+import cn.edu.gzmu.center.model.DATABASE
+import cn.edu.gzmu.center.model.database
+import cn.edu.gzmu.center.model.databaseInt
 import io.vertx.core.Promise
+import io.vertx.core.json.JsonObject
+import io.vertx.kotlin.core.eventbus.requestAwait
+import io.vertx.kotlin.core.json.jsonObjectOf
 import io.vertx.kotlin.coroutines.CoroutineVerticle
 import io.vertx.kotlin.sqlclient.getConnectionAwait
 import io.vertx.kotlin.sqlclient.queryAwait
+import io.vertx.pgclient.PgConnectOptions
 import io.vertx.pgclient.PgPool
+import io.vertx.sqlclient.PoolOptions
 import kotlinx.coroutines.launch
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
@@ -32,4 +41,24 @@ class DatabaseVerticle : CoroutineVerticle() {
       log.error("Failed start database verticle!", e.cause)
     }
   }
+
+  private fun databaseConfig(config: JsonObject): PgConnectOptions =
+    PgConnectOptions(
+      jsonObjectOf(
+        "port" to config.getInteger("port", 5432),
+        "host" to config.getString("host", "127.0.0.1"),
+        "database" to config.getString("database", "public"),
+        "user" to config.getString("user", "postgres"),
+        "password" to config.getString("password", "postgres")
+      )
+    )
+
+  private fun poolConfig(config: JsonObject): PoolOptions =
+    PoolOptions(
+      jsonObjectOf(
+        "maxSize" to config.getInteger("maxSize", 5),
+        "maxWaitQueueSize" to config.getInteger("maxWaitQueueSize", -1)
+      )
+    )
+
 }
